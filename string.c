c26f16066facf801c4ab363a42a273ac15aa11bd
Chiuzorana <florent.ace307@gmail.com>
2019-10-09 14:03:33 +0800

    zorana test

--- string.c
+++ string.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-
+/*20191009*/
 size_t my_strlen(char *s)
 {
 	char *p = s;
 	while (*p) ++p;
 	return (p - s);
 }
-
 char *my_strcpy(char *t, char *s)
 {
 	char *p = t; 
 	while (*t ++ = *s ++);
 	return p;
 }
-
 int main(void) {
 	int i;
 
