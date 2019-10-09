9e36a34e57025174437ad80605066e228529a776
yanling0711 <yanlingwang0711@gmail.com>
2019-10-09 13:48:40 +0800

    Add my_strcpy fuction

--- string.c
+++ string.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
-size_t my_strlen(const char *s)
+size_t my_strlen(char *s)
 {
 	char *p = s;
 	while (*p) ++p;
 	return (p - s);
+}
+
+char *my_strcpy(char *t, char *s)
+{
+	char *p = t; 
+	while (*t ++ = *s ++);
+	return p;
 }
 
 int main(void) {
@@ -15,6 +22,8 @@ int main(void) {
 	};
 	for (i = 0; i < 2; ++i)
 		printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+
+        printf("%s\n", my_strcpy(p1, "Hello, World !!!"));
 	
 	return 0;
 }
